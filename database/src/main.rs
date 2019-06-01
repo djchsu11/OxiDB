880c32219dcdf1ce73336be8b6eb9b091db8ca8e
djchsu11 <or.no.world@gmail.com>
2019-05-31 19:54:16 -0500

    Refactored main to be nicer

--- database/src/main.rs
+++ database/src/main.rs
@@ -1,55 +1,46 @@
 use std::io;
+
 use core;
+
 mod constants;
 mod kinds;
 
-use kinds::*;
 
 fn main() {
     loop{
         let input = get_user_input();
-        let mut a = MetaCommandKind::MetaCommandUnrecognizedCommand;
-        let mut first_char = ' ';
-        let mut status = ExecutionStatusKind::ExitFailure;
 
-        if !input.is_empty(){
-            first_char = input.chars().next().unwrap();
+        if input.is_empty() {
+            println!("Please enter a command.");
+            continue;
         }
 
-        if first_char == '.'{
-            a = MetaCommandKind::MetaCommandSuccess;
-        }
-        match a {
-            MetaCommandKind::MetaCommandSuccess =>
-                status = execute_command(input),
-            MetaCommandKind::MetaCommandUnrecognizedCommand =>
-                println!("That's not a command..."),
-        }
+        let first_char = input.chars().next().unwrap();
+        let command_type = get_command_type(first_char);
+        let status = do_command(command_type, input);
 
-        if status == ExecutionStatusKind::ExitSuccess{
+        if status == kinds::ExecutionStatusKind::ExitSuccess {
             println!("Exiting...");
             break;
         }
     }
 }
 
-fn execute_command(command: String) -> ExecutionStatusKind{
-    if command.to_ascii_lowercase() == constants::EXIT_COMMAND{
-        ExecutionStatusKind::ExitSuccess
-
-    }else if command.to_ascii_lowercase() == constants::MAKE_INSERT{
+fn execute_command(command: String) -> kinds::ExecutionStatusKind {
+    if command.to_ascii_lowercase() == constants::EXIT_COMMAND.to_string() {
+        kinds::ExecutionStatusKind::ExitSuccess
+    } else if command.to_ascii_lowercase() == constants::MAKE_INSERT.to_string() {
         core::test();
-        ExecutionStatusKind::ExitSuccess
+        kinds::ExecutionStatusKind::ExitSuccess
 
     }else{
         println!("Unrecognized command: {}", command);
-        ExecutionStatusKind::ExitFailure
+        kinds::ExecutionStatusKind::ExitFailure
     }
 }
 
 fn get_user_input() -> String{
     let mut input = String::new();
-    input.clear();
 
     println!("Type a command:");
     io::stdin().read_line(&mut input).expect("Please enter a command.");
@@ -58,4 +49,22 @@ fn get_user_input() -> String{
 
 }
 
+fn get_command_type(command_char: char) -> kinds::MetaCommandKind {
+    if command_char == '.' {
+        kinds::MetaCommandKind::MetaCommandSuccess
+    } else {
+        kinds::MetaCommandKind::MetaCommandUnrecognizedCommand
+    }
+}
 
+fn do_command(command_type: kinds::MetaCommandKind, input: String) -> kinds::ExecutionStatusKind {
+    match command_type {
+        kinds::MetaCommandKind::MetaCommandSuccess =>
+            execute_command(input),
+        kinds::MetaCommandKind::MetaCommandUnrecognizedCommand =>
+            {
+                println!("That's not a command...");
+                kinds::ExecutionStatusKind::ExitFailure
+            },
+    }
+}
